4c85d5feaa12da1cb7a63578b07f0e315edbb11d
Pavel Vynychenko <pavel.vynych@gmail.com>
2020-08-26 07:38:32 +0300

    store update

--- src/store/actions/cartActions.js
+++ src/store/actions/cartActions.js
@@ -4,6 +4,9 @@ import {
 	GET_ITEMS_SUCCESS,
 	GET_ITEMS_FAILURE,
 	GET_ITEMS_STARTED,
+	GET_ITEM_SUCCESS,
+	GET_ITEM_FAILURE,
+	GET_ITEM_STARTED,
 	ADD_ITEM,
 	INCREMENT_ITEM,
 	DECREMENT_ITEM,
@@ -13,35 +16,69 @@ import {
 
 export const GET_ITEMS_ACTION = () => {
 	return dispatch => {
-		dispatch(addTodoStarted());
+		dispatch(addItemsStarted());
 
 		axios
-			.get(`https://yalantis-react-school-api.yalantis.com/api/v1/products?perPage=200&origins=`)
+			.get(`https://yalantis-react-school-api.yalantis.com/api/v1/products?perPage=200`)
 			.then(res => {
-				dispatch(addTodoSuccess(res.data.items));
+				dispatch(addItemsSuccess(res.data.items));
 			})
 			.catch(err => {
-				dispatch(addTodoFailure(err.message));
+				dispatch(addItemsFailure(err.message));
 			});
 
 	};
 };
 
-const addTodoSuccess = items => ({
+const addItemsSuccess = items => ({
 	type: GET_ITEMS_SUCCESS,
 	payload: items
 });
 
-const addTodoStarted = () => ({
+const addItemsStarted = () => ({
 	type: GET_ITEMS_STARTED
 });
 
-const addTodoFailure = error => ({
+const addItemsFailure = error => ({
 	type: GET_ITEMS_FAILURE,
 	payload: {
 		error
 	}
 });
+
+
+export const GET_ITEM_ACTION = (id) => {
+	return dispatch => {
+		dispatch(addItemStarted());
+
+		axios
+			.get(`https://yalantis-react-school-api.yalantis.com/api/v1/products/${id}`)
+			.then(res => {
+				dispatch(addItemSuccess(res.data));
+			})
+			.catch(err => {
+				dispatch(addItemFailure(err.message));
+			});
+
+	};
+};
+
+const addItemSuccess = curentItem => ({
+	type: GET_ITEM_SUCCESS,
+	payload: curentItem
+});
+
+const addItemStarted = () => ({
+	type: GET_ITEM_STARTED
+});
+
+const addItemFailure = error => ({
+	type: GET_ITEM_FAILURE,
+	payload: {
+		error
+	}
+});
+
 
 export const ADD_ITEM_ACTION = (item) => (dispatch) => {
 	dispatch({
--- src/store/reducers/itemsReducer.js
+++ src/store/reducers/itemsReducer.js
@@ -1,5 +1,6 @@
 import {
 	GET_ITEMS_SUCCESS,
+	GET_ITEM_SUCCESS,
 	ADD_ITEM,
 	INCREMENT_ITEM,
 	DECREMENT_ITEM,
@@ -9,6 +10,7 @@ import { add, decrement, increment, remove } from "../../utils";
 
 const initialState = {
 	items: [],
+	curentItem: [],
 	cartList: [],
 };
 
@@ -16,6 +18,9 @@ export default function (state = initialState, action) {
 	switch (action.type) {
 		case GET_ITEMS_SUCCESS:
 			return { ...state, items: action.payload };
+
+		case GET_ITEM_SUCCESS:
+			return { ...state, curentItem: action.payload };
 
 		case ADD_ITEM: {
 			return { ...state, cartList: add(state, action.payload.id) };
